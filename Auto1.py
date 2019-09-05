78dbee5135711dcd354d28e8be711ae737641245
Deva Chaitanya Sanakkayala <deva.chaitanya.btech2018@sitpune.edu.in>
2019-09-06 02:27:48 +0530

    Add files via upload

---
+++ Auto1.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  9 21:15:55 2019
+
+@author: devac
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  5 12:17:12 2019
+
+@author: devac
+"""
+import time
+import pywinauto
+#import win32api
+from pywinauto import application
+import tkinter
+import clipboard
+
+def Xconv(x):
+    x=int((x / 1366 * tkinter.Tk().winfo_screenwidth()))
+    return(x)
+def Yconv(y):
+    y=int((y / 768 * tkinter.Tk().winfo_screenheight()))
+    return(y)
+    
+
+app = application.Application()
+app.start("E:\\ESSL\\Smart Office suite.exe")
+time.sleep(5)
+SMRTOFF = app.window(title='SmartOffice Login')
+time.sleep(1)
+SMRTOFF.Edit2.set_edit_text('smart')
+SMRTOFF.Edit1.set_edit_text('smart')
+SMRTOFF.Login.click()
+SMRTOFF1=app.window(title='Smart Office Suite 9.5')
+SMRTOFF1.wait('Ready')
+SMRTOFF1.maximize()
+time.sleep(2)
+pywinauto.mouse.click(button='left', coords=(882,56))
+time.sleep(2)
+SMRTOFF2=app.window(title='Smart Office Suite 9.5 - [Device List]')
+SMRTOFF2.Button.click_input()
+SMRTOFF2.Button14.click_input()
+time.sleep(2)
+flag=1
+for i in range(2):
+    stime=time.time()
+    #print(time.time()-stime)
+    while (time.time() - stime)<=600:
+        time.sleep(10)
+        pywinauto.mouse.click(button='left', coords=(782,170+20*i))
+        pywinauto.keyboard.send_keys('^c')
+        text=clipboard.paste()
+        print(text.find('Downloading Completed'))
+        print("loop")
+        if text.find('Downloading Completed')!=-1:
+            flag=0
+            print('Downloading Completed')
+            break
+        elif text.find('Unable To Connect')!=-1:
+            print(flag)
+            flag=1
+            break
+
+if flag==0:
+    app.kill()
+    app1 = application.Application()
+    app1.start("E:\\ESSL\\WebRun.exe")
+elif flag==1:
+    print('Unable to connect') 
+
+#print(win32api.GetCursorPos())
+
+
+    
+    
---
+++ AutoDevice.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  5 12:17:12 2019
+
+@author: devac
+"""
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from datetime import date,timedelta
+
+def datec():
+    dt = date.today()
+    if date.weekday(dt) == 0 :
+       dt=date.today()-timedelta(days=3)
+       dt=dt.strftime("%d %b %Y")
+       #print(dt)
+       return(dt)
+    else:
+       dt=date.today()-timedelta(days=1)
+       dt=dt.strftime("%d %b %Y")
+       #print(dt)
+       return(dt)
+print(datec())
+def press(a):
+    elem=browser.find_element_by_xpath(a)
+    elem.get_attribute('href')
+    elem.click()
+    
+  #UPLOAD
+print("Make sure chrome webdriver is located in C:\\chromedriver_win32\\chromedriver.exe'")
+browser=webdriver.Chrome('E:\\chromedriver_win32\\chromedriver.exe')
+browser.get('http://10.46.10.114:9001/login')
+browser.maximize_window()
+time.sleep(10)
+UName=browser.find_element_by_name('user')#inspect element
+UName.send_keys(Keys.CONTROL,'a')
+UName.send_keys('attAdmin')
+PWord=browser.find_element_by_name('password') #inspect element
+PWord.send_keys(Keys.CONTROL,'a')
+PWord.send_keys("Attadmin@123")
+PWord.send_keys(Keys.ENTER)
+time.sleep(15)
+browser.get('http://10.46.10.114:9001/home')
+press('/html/body/div[1]/div/div/div/ul/li[1]/a')
+time.sleep(5)
+browser.get('http://10.46.10.114:9001/setting')
+press('//*[@id="loader-setting"]/div/div[1]/ul/li[3]/a')#inspect element
+time.sleep(1)
+press('//*[@id="mainDiv"]/div[2]/div/table/tbody/tr[8]/td/span')
+Edate=browser.find_element_by_name('date')
+Edate.send_keys(datec())
+Edate.send_keys(Keys.ENTER)
+time.sleep(1)
+Edate.send_keys(Keys.ENTER)
+time.sleep(3)
+browser.close() 
+                          
+ #LOAD
+browser=webdriver.Chrome('E:\\chromedriver_win32\\chromedriver.exe')
+browser.get('https://machint.greythr.com/')
+browser.maximize_window()
+time.sleep(20)
+UName=browser.find_element_by_name('username')
+UName.send_keys(Keys.CONTROL,'a')
+UName.send_keys('Test')
+PWord=browser.find_element_by_name('password') #inspect element
+PWord.send_keys(Keys.CONTROL,'a')
+PWord.send_keys("Test@12345")
+PWord.send_keys(Keys.ENTER)
+time.sleep(15)
+browser.get('https://machint.greythr.com/home.do#/')
+press('/html/body/div[5]/div[1]/div/div[1]/ul/li[2]/a')
+press('/html/body/div[5]/div[1]/div/div[2]/ul/li[2]/ul/li[3]/a/span')
+press('/html/body/div[5]/div[1]/div/div[2]/ul/li[2]/ul/li[3]/ul/li[1]/a')
+time.sleep(5)
+browser.get('https://machint.greythr.com/ngapp/leave/admin/attendanceprocessor')
+press('//*[@id="app-container"]/div/div[2]/div/div[2]/span')
+fdate=browser.find_element_by_name('fromDate')
+fdate.send_keys(Keys.CONTROL,'a')
+fdate.send_keys(datec())
+tdate=browser.find_element_by_name('toDate')
+tdate.send_keys(Keys.CONTROL,'a')
+tdate.send_keys(datec())
+time.sleep(1)
+tdate.send_keys(Keys.TAB)
+time.sleep(1)
+button=browser.find_element_by_css_selector('body > div.modal.ng-scope > div.modal-footer > button.btn.btn-medium.btn-primary')
+button.click()
+time.sleep(300)
+browser.close()
+
